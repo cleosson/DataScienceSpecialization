fef90a7de8f3d00a4e396f00f1ba919f0369604c
Cleosson Souza <cleosson@hotmail.com>
2015-07-11 19:42:10 -0300

    code project 1

--- plot3.R
+++ plot3.R
@@ -26,6 +26,6 @@ with(data, plot(FullDate, Sub_metering_1, type = "n", ylab = "Energy sub meterin
 with(data, points(FullDate, Sub_metering_1, col = "black", type = "l")) 
 with(data, points(FullDate, Sub_metering_2, col = "red", type = "l")) 
 with(data, points(FullDate, Sub_metering_3, col = "blue", type = "l")) 
-legend("topright", lty=1, lwd=2, bty="n", col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+legend("topright", lty=1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 dev.off()
